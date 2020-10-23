fce243e7a0909a44b715e855d8b1239487148087
giljun <giljune91@gmail.com>
2020-10-23 15:49:48 +0900

    2020/10/23 Quiz_3.py

---
+++ 나도코딩-파이썬-기본/20201023.py
@@ -0,0 +1,62 @@
+# 파일 입출력_파일 입력하기
+# score_file = open("score.txt", "w", encoding="utf8")
+# print("수학 : 0", file=score_file)
+# print("영어 : 50", file=score_file)
+# score_file.close()
+
+# 파일 입출력_파일 입력 추가히가
+# score_file = open("score.txt", "a", encoding="utf8")
+# score_file.write("과학 : 80")
+# score_file.write("\n코딩 : 100")
+# score_file.close()
+
+# 파일 입출력_파일 읽어오기
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.read())
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.readline())  # 줄별로 읽기, 한 줄 읽고 커서는 다음 줄로 이동
+# print(score_file.readline())  # 줄별로 읽기, 한 줄 읽고 커서는 다음 줄로 이동
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# while True:
+#     line = score_file.readline()
+#     if not line:
+#         break
+#     print(line)
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# lines = score_file.readlines()
+# for line in lines:
+#     print(line, end="")
+
+# score_file.close()
+
+# pickle 사용법
+# import pickle
+# profile_file = open("profile.pickle", "wb")
+# profile = {"name": "Giljun", "age": 30,
+#            "hobby": ["pingpong", "golf", "stocks"]}
+# print(profile)
+# pickle.dump(profile, profile_file)  # profile에 있는 정보를 file에 저장
+# profile_file.close()
+
+# profile_file = open("profile.pickle", "rb")
+# profile = pickle.load(profile_file)  # file에 있는 정보를 profile에 불러오기
+# print(profile)
+# profile_file.close()
+
+# with 사용법
+import pickle
+
+# with open("profile.pickle", "rb") as profile_file:
+#     print(pickle.load(profile_file))
+
+# with open("study.txt", "w", encoding="utf8") as study_file:
+#     study_file.write("파이썬을 열심히 공부하고 있어요")
+
+with open("study.txt", "r", encoding="utf8") as study_file:
+    print(study_file.read())
