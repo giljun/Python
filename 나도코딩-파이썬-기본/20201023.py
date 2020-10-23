1dea84469df4f30dff78a1be0f3de5d9e534832d
giljun <giljune91@gmail.com>
2020-10-23 15:51:19 +0900

    2020/10/23 modify 20201023.py and Quiz_3.py

--- 나도코딩-파이썬-기본/20201023.py
+++ 나도코딩-파이썬-기본/20201023.py
@@ -58,5 +58,5 @@ import pickle
 # with open("study.txt", "w", encoding="utf8") as study_file:
 #     study_file.write("파이썬을 열심히 공부하고 있어요")
 
-with open("study.txt", "r", encoding="utf8") as study_file:
-    print(study_file.read())
+# with open("study.txt", "r", encoding="utf8") as study_file:
+#     print(study_file.read())
--- 나도코딩-파이썬-기본/Quiz_3.py
+++ 나도코딩-파이썬-기본/Quiz_3.py
@@ -1,29 +1,18 @@
-# Quiz) 표준 체중을 구하는 프로그램을 작성하시오
+# Quiz) 당신의 회사에서는 매주 1회 작성해야 하는 보고서가 있습니다.
+# 보고서는 항상 아래와 같은 형태로 출력되어야 합니다.
 
-# 표준 체중 : 각 개인의 키에 적당한 체중
+# - X 주차 주간보고 -
+# 부서 :
+# 이름 :
+# 업무 요약 :
 
-# (성별에 따른 공식)
-# 남자: 키(m) x 키(m) x 22
-# 여자: 키(m) x 키(m) x 21
+#  1주차부터 50주차까지의 보고서 파일을 만드는 프로그램을 작성하시오.
 
-# 조건 1 : 표준 체중은 별도의 함수 내에서 계산
-# * 함수명 : std_weight
-# * 전달값 : 키(height), 성별(gender)
+# 조건 : 파일명은 '1주차.txt', '2주차.txt', ... 와 같이 만듭니다.
 
-# 조건 2 : 표준 체중은 소수점 둘째자리까지 표시
-
-# (출력 예제)
-# 키 175cm 남자의 표준 체중은 67.38kg 입니다.
-
-def std_weight(height, gender):
-    if gender == "남자":
-        return height * height * 22
-    else:
-        return height * height * 21
-
-
-height = 175
-gender = "남자"
-weight = round(std_weight(height/100, gender), 2)
-
-print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender, weight))
+for i in range(1, 51):
+    with open(str(i) + "주차.txt", "w", encoding="utf8") as report_file:
+        report_file.write("- {0} 주차 주간보고 -".format(i))
+        report_file.write()
+        report_file.write()
+        report_file.write()
