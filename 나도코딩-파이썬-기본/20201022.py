02cde5772a912933ad41db88dd6093bbd8ea2fd8
giljun <giljune91@gmail.com>
2020-10-23 13:28:40 +0900

    2020/10/23 나도코딩-파이썬-기본 다양한 출력 포맷

--- 나도코딩-파이썬-기본/20201022.py
+++ 나도코딩-파이썬-기본/20201022.py
@@ -1,13 +1,68 @@
-def profile(name, age, main_lang):
-    print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}"
-          .format(name, age, main_lang))
+# def profile(name, age, main_lang):
+#     print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}"
+#           .format(name, age, main_lang))
 
 
-profile("유재석", 20, "파이썬")
-profile("김태호", 25, "자바")
+# profile("유재석", 20, "파이썬")
+# profile("김태호", 25, "자바")
 
 # 같은 학교 같은 학년 같은 반 같은 수업.
 
 
 # def profile(name, age, main_lang):
-#     print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}" \.format(name, age, main_lang))
+#     print(name, age, main_lang)
+
+
+# profile(name="유재석", main_lang="파이썬", age=20)
+# profile(main_lang="자바", age=25, name="김태호")
+
+# def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+#     print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
+#     print(lang1, lang2, lang3, lang4, lang5)
+
+# 가변인자
+# 함수의 매개변수에 *를 추가하면 다음과 같이 사용할 수 있다.
+# def profile(name, age, *language):
+#     print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
+#     for lang in language:
+#         print(lang, end=" ")
+#     print()
+
+
+# profile("유재석", 20, "Python", "Java", "C", "C++", "C#")
+# profile("김태호", 25, "Kotlin", "Swift", "", "", "")
+
+# 지역변수와 전역변수
+# import sys
+# gun = 10
+
+
+# def checkpoint(soldiers):  # 경계근무
+#     global gun  # 전역 공간에 있는 gun 사용
+#     gun = gun - soldiers
+#     print("[함수 내] 남은 총 : {0}".format(gun))
+
+
+# def checkpoint_ret(gun, soldiers):
+#     gun = gun - soldiers
+#     print("[함수 내] 남은 총 : {0}".format(gun))
+#     return gun
+
+
+# print("전체 총 : {0}".format(gun))
+# # checkpoint(2)
+# gun = checkpoint_ret(gun, 2)
+# print("남은 총 : {0}".format(gun))
+
+
+# 표준 입출력
+# print("Python", "Java", file=sys.stdout)
+# print("Python", "Java", file=sys.stdout)
+
+# scores = {"수학": 0, "영어": 50, "코딩": 100}
+# for subject, score in scores.items():
+#     print(subject.ljust(5), str(score).rjust(4), sep=":")
+
+# 다양한 입출력
+# 빈 자리는 빈공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
+print("{0: >10}".format(500))
